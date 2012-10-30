87e8a53ec20599ff3003e5cb05e83be2d3409935
边钢 <macisi528@gmail.com>
2012-10-30 15:12:41 +0800

    增加取色板功能

--- js/main.js
+++ js/main.js
@@ -5,6 +5,21 @@ var Draw = Draw || {};
             wrap = doc.getElementById(id);
         wrap.innerHTML = _.template(tpl, data);
     };
+
+    var prop = {
+            color: "#000",
+            shape: "line",
+            width: 1,
+            fill: "#fff",
+            lock: false
+        },
+        line,
+        rect,
+        circle,
+        colorStatus = doc.getElementById("Js_color"),
+        foreColor = colorStatus.querySelector(".foreground"),
+        backColor = colorStatus.querySelector(".background");
+
     var stage = new Kinetic.Stage({
         container: "Js_canvas",
         width: 960,
@@ -21,45 +36,27 @@ var Draw = Draw || {};
     layer.add(background);
     stage.add(layer);
 
-    Draw.start = function(){
-        var moving = false,
-            prop = {
-                color: "#000",
-                shape: "line",
-                width: 1,
-                fill: "#fff"
-            },
-            pos = [],
-            line,
-            rect,
-            circle,
-            colorStatus = doc.getElementById("Js_color"),
-            foreColor = colorStatus.querySelector(".foreground"),
-            backColor = colorStatus.querySelector(".background");
-        doc.getElementById("Js_palette").addEventListener("mousedown", function(evt){
-            var color = evt.target.dataset.color;
-            if(color) {
-                if (evt.button === 0) {
-                    prop.color = color;
-                    foreColor.style.backgroundColor = color;
+    doc.getElementById("Js_shape").addEventListener("click", function(evt){
+        var shape = evt.target.dataset.shape;
+        if(shape) {
+            if (shape === "eraser") {
+                prop.shape = "line";
+                prop.color = "#fff";
+                prop.lock = true;
+                return;
+            }
+            prop.shape = shape;
+            prop.lock = false;
+        }
+    }, false);
+    doc.getElementById("Js_strokeWidth").addEventListener("change", function(){
+        prop.width = this.value;
+    }, false);
 
-                } else if(evt.button === 2) {
-                    prop.fill = color;
-                    backColor.style.backgroundColor = color;
-                    evt.target.oncontextmenu = function(){
-                        return false; //屏蔽右键菜单
-                    };
-                }
-            }
-        }, false);
-        doc.getElementById("Js_shape").addEventListener("click", function(evt){
-            if(evt.target.dataset.shape) {
-                prop.shape = evt.target.dataset.shape;
-            }
-        }, false);
-        doc.getElementById("Js_strokeWidth").addEventListener("change", function(evt){
-            prop.width = this.value;
-        }, false);
+    Draw.init = function(){
+        var moving = false,
+            pos = [];
+
         stage.on("mousedown", function(){
             if (moving){
                 moving = false;
@@ -69,6 +66,7 @@ var Draw = Draw || {};
                 switch(prop.shape) {
                     case "line":
                     default :
+                        prop.color = prop.lock ? "#fff": prop.color;
                         line = new Kinetic.Line({
                             points: [mousePos.x, mousePos.y, mousePos.x, mousePos.y],
                             stroke: prop.color,
@@ -135,7 +133,121 @@ var Draw = Draw || {};
             moving = false;
             pos = [];
         });
-    }
+    };
+
+    var palette = (function(){
+        var color,
+            isIn = false;
+        var paletteStage = new Kinetic.Stage({
+            container: "Js_palette",
+            width: 480,
+            height: 320
+        });
+        var barLayer = new Kinetic.Layer();
+        var maskLayer = new Kinetic.Layer();
+        var bar = new Kinetic.Rect({
+            x: 50,
+            y: 30,
+            width: 20,
+            height: 256,
+            stroke: "#000",
+            fill: {
+                start: {
+                    x: 0,
+                    y: 2
+                },
+                end: {
+                    x: 0,
+                    y: 254
+                },
+                colorStops: [0, '#f00', 1/6, "#f0f", 1/3, '#00f', 1/2, '#0ff', 2/3, '#0f0', 5/6, '#ff0', 1, '#f00']
+            }
+        });
+        var blackMask = new Kinetic.Rect({
+            x: 80,
+            y: 30,
+            width: 256,
+            height: 256,
+            stroke: "#000",
+            fill: {
+                start: {
+                    x: 0,
+                    y: 2
+                },
+                end: {
+                    x: 0,
+                    y: 254
+                },
+                colorStops: [0, 'rgba(0, 0, 0, 0)', 1, 'rgba(0, 0, 0, 1)']
+            }
+        });
+        var whiteMask = new Kinetic.Rect({
+            x: 80,
+            y: 30,
+            width: 256,
+            height: 256,
+            stroke: "#000",
+            fill: {
+                start: {
+                    x: 2,
+                    y: 0
+                },
+                end: {
+                    x: 254,
+                    y: 0
+                },
+                colorStops: [0, 'rgba(255, 255, 255, 1)', 1, 'rgba(255, 255, 255, 0)']
+            }
+        });
+        var colorMask = new Kinetic.Rect({
+            x: 80,
+            y: 30,
+            width: 256,
+            height: 256,
+            stroke: "#000",
+            fill: "#f00"
+        });
+        barLayer.add(bar);
+        maskLayer.add(colorMask);
+        maskLayer.add(whiteMask);
+        maskLayer.add(blackMask);
+        paletteStage.add(barLayer);
+        paletteStage.add(maskLayer);
+        bar.on("mousedown mousemove", function(evt){
+            if (isIn || evt.type === "mousedown") {
+                isIn = true;
+                color = getColor.call(this, paletteStage.getMousePosition());
+                if (color !== "rgb(0,0,0)") {
+                    colorMask.setFill(color);
+                }
+                maskLayer.drawScene();
+            }
+        });
+        bar.on("mouseup", function(){
+            isIn = false;
+        });
+
+
+        maskLayer.on("mousedown", function(evt){
+            color = getColor.call(this, paletteStage.getMousePosition());
+            console.log(color);
+            if (evt.button === 0) {
+                prop.color = color;
+                foreColor.style.backgroundColor = color;
+            } else if(evt.button === 2) {
+                prop.fill = color;
+                backColor.style.backgroundColor = color;
+                evt.target.oncontextmenu = function(){
+                    return false; //屏蔽右键菜单
+                };
+            }
+        });
+        function getColor(pos) {
+            var data = this.getContext().getImageData(pos.x, pos.y, 1, 1).data;
+            data = _.first(data, 3).toString();
+            return "rgb(" + data + ")";
+        }
+    })();
     return Draw;
 })(document);
 
