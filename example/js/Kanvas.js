dcf3954ab4c99e6a5243cca5911649f21b2c7364
边钢 <macisi528@gmail.com>
2012-12-04 17:26:54 +0800

    增加3d方法和测试

--- example/js/Kanvas.js
+++ example/js/Kanvas.js
@@ -60,11 +60,73 @@ Kanvas = (function(w){
         };
     }
 
+    var _3D = {
+        rotateXAngle: 0,
+        rotateYAngle: 0,
+        rotateZAngle: 0,
+        rotateRAngle: Math.PI / 8,
+        rotateX: function(x, y, z, a) {
+            return {
+                x: x,
+                y: y * Math.cos(a) - z * Math.sin(a),
+                z: y * Math.sin(a) + z * Math.cos(a)
+            }
+        },
+        rotateY: function(x, y, z, a) {
+            return {
+                x: x * Math.cos(a) + z * Math.sin(a),
+                y: y,
+                z: z * Math.cos(a) - x * Math.sin(a)
+            };
+        },
+        rotateZ: function(x, y, z, a) {
+            return {
+                x: x * Math.cos(a) - y * Math.sin(a),
+                y: x * Math.sin(a) + y * Math.cos(a),
+                z: z
+            };
+        },
+        /**
+         * 球面坐标系转直角坐标系
+         * @param a 仰角
+         * @param b 转角
+         * @param r 半径
+         * @return {Object}
+         */
+        spherical: function(a, b, r) {
+            return {
+                x: r * Math.sin(a) * Math.cos(b + this.rotateRAngle),
+                y: r * Math.sin(a) * Math.sin(b + this.rotateRAngle),
+                z: r * Math.cos(a)
+            }
+        },
+        /**
+         * 投影到2d坐标
+         * @param x
+         * @param y
+         * @param z
+         * @return {Object}
+         */
+        projection: function(x, y, z) {
+            var c1 = this.rotateX(x, y, z, this.rotateXAngle);
+            var c2 = this.rotateY(c1.x, c1.y, c1.z, this.rotateYAngle);
+            var c3 = this.rotateZ(c2.x, c2.y, c2.z, this.rotateZAngle);
+
+            //平行投影至XOY平面
+            //return {x: c3.x, y: c3.y};
+
+            //透视投影至XOY平面，观察点(0, 0, 1000)
+            var h = 1 - c3.z / 1000;
+            return {x: c3.x / h , y: c3.y / h};
+        }
+    };
+
     return {
         getCanvas: getCanvas,
         requestAnimationFrame: getAnimationFrame,
         cancelAnimationFrame: cancelAnimationFrame,
-        getMousePosition: getMousePos
+        getMousePosition: getMousePos,
+        _3D: _3D
     }
 
 }(window));
